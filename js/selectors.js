1e05f7a46a7b0da666598e2a09f61d4cf9946e50
Jean-Patrice <jpjocegilbert@gmail.com>
2018-08-22 16:17:01 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -7,3 +7,15 @@ function paragraphSelector(){
 function lastImageSelector(){
   return $('img:last')
 }
+
+function ninjaBabySelector(){
+  return $('img#baby-ninja')
+}
+
+function divSelector(){
+  return $('div.pics')
+}
+
+function firstListItem(){
+  return $('ul#pic-list li:first-child')
+}
