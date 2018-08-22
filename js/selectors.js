e6f6838c759d7d3f38508a27d18f4f4fc9e1f3c5
Jean-Patrice <jpjocegilbert@gmail.com>
2018-08-22 15:59:25 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,9 @@
 // declare your functions here...
+//function should use an element selector to select the p tags in index.html and return those elements.
+function paragraphSelector(){
+  return $('p')
+}
+
+function lastImageSelector(){
+  return $('img:last')
+}
